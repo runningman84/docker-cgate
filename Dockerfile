68e4ffba72b6072c5e2b5ffb86ce95b8c9b777ca
Philipp Hellmich <phil@hellmi.de>
2016-01-11 21:58:44 +0100

    Added initial cgpav support, fixed filesystem rights

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER Philipp Hellmich <phil@hellmi.de>
 #RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
 # install wget
-RUN yum install -y wget
+RUN yum install -y wget tar make gcc
 
 # install dumb init
 RUN wget -q https://github.com/Yelp/dumb-init/releases/download/v1.0.0/dumb-init_1.0.0_amd64 \
@@ -23,11 +23,21 @@ RUN wget -q ftp://ftp.stalker.com/pub/CommuniGatePro/CGatePro-Linux.x86_64.rpm \
 && rpm -i /tmp/CGatePro-Linux.x86_64.rpm \
 && rm /tmp/CGatePro-Linux.x86_64.rpm
 
+# install cgpav
+# http://program.farit.ru/antivir/cgpav-1.5.tar.gz
+ADD cgpav /opt/CommuniGate/cgpav
+RUN chmod 755 /opt/CommuniGate/cgpav
+
+ADD cgpav.conf /etc/cgpav.conf
+
+ADD run.sh /run.sh
+RUN chmod +x /*.sh
+
 # Define mountable directories.
 VOLUME ["/var/CommuniGate"]
 
 # Server CMD
-CMD ["dumb-init", "/opt/CommuniGate/CGServer", "--Base /var/CommuniGate", "--dropRoot", "--logToConsole"]
+CMD ["dumb-init", "/run.sh"]
 
 # Expose ports.
 # Webadmin http/https
---
+++ run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+APPLICATION="/opt"
+BASEFOLDER="/var/CommuniGate"
+SUPPLPARAMS="--dropRoot --logToConsole"
+
+echo "=> Using the following CommuniGatePro configuration:"
+echo "========================================================================"
+echo "      Application folder:  $APPLICATION"
+echo "      Data folder:         $BASEFOLDER"
+echo "      Startup parameters:  $SUPPLPARAMS"
+echo ""
+echo " Content filtering can be done with clamav and spamd"
+echo " use this as a content filter: "
+echo " /opt/CommuniGate/cgpav -f /etc/cgpav.conf"
+echo "========================================================================"
+
+
+[ -f ${APPLICATION}/CommuniGate/CGServer ] || exit 1
+
+#ulimit -u 2000
+ulimit -c 2097151
+umask 0
+
+# Custom startup parameters
+if [ -f ${BASEFOLDER}/Startup.sh ]; then
+  . ${BASEFOLDER}/Startup.sh
+fi
+
+if [ -d ${BASEFOLDER} ] ; then
+  # ...
+  chown -R root.mail ${BASEFOLDER}
+  chmod -R g+rw ${BASEFOLDER}
+else
+  echo "Creating the CommuniGate Base Folder..."
+  mkdir ${BASEFOLDER}
+  chgrp mail ${BASEFOLDER}
+  chmod 2770 ${BASEFOLDER}
+fi
+
+#sed -i /var/CommuniGate/cgpav.conf "s/spamassassin_host =.*/spamassassin_host=xxx/g"
+
+echo "Starting CommuniGate Pro"
+
+exec ${APPLICATION}/CommuniGate/CGServer --Base ${BASEFOLDER} ${SUPPLPARAMS}
